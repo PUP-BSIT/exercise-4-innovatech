b7b65493f6a130d6ba0eed7e7971ef07763bc22b
Kyla Rica Malaluan <malaluankylarica@gmail.com>
2024-04-30 15:45:13 +0800

    create add button and function

--- exercise_4/src/app/parent/parent.component.ts
+++ exercise_4/src/app/parent/parent.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../../model/product';
 
 @Component({
   selector: 'app-parent',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './parent.component.css'
 })
 export class ParentComponent {
+  products:Product[] = [
+    {id: 1, product_name: "Jeno PC", price: 250},
+    {id: 2, product_name: "Jaemin Album", price: 450},
+  ];
 
+  newProduct: Product = {id: null, product_name: '', price: null};
+
+  addProduct() {
+    const newId = this.products.length + 1;
+    const newProduct: Product = {id: newId, product_name:this.newProduct.product_name, price:this.newProduct.price};
+
+    this.products.push(newProduct);
+  
+    this.newProduct.product_name= '';
+    this.newProduct.price= null;
+  }
 }
